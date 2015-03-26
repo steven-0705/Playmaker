0386ad503a41d00b5537a563bb2752dcc6d0e109
unknown <chrisf671@gmail.com>
2015-03-26 17:59:23 -0500

    Added the base for our servlet as well as adding some android classes for accessing the database

---
+++ MyApplication/app/src/main/java/casuals/filthy/playmaker/data/DatabaseController.java
@@ -0,0 +1,21 @@
+package casuals.filthy.playmaker.data;
+
+/**
+ * Created by Chris on 3/26/2015.
+ */
+public class DatabaseController {
+
+    public static final String ServletURL = "http://playmaker-app.appspot.com/hello";
+
+    private static DatabaseController single;
+
+    private DatabaseController () {
+
+    }
+
+
+
+
+
+
+}
