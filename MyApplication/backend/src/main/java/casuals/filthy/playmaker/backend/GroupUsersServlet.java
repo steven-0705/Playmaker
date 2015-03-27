012357e4a433ebdad68a95cbb4cdd5b62bbd41d9
unknown <chrisf671@gmail.com>
2015-03-27 15:05:56 -0500

    Added new servlets and updated web.xml

---
+++ MyApplication/backend/src/main/java/casuals/filthy/playmaker/backend/GroupUsersServlet.java
@@ -0,0 +1,9 @@
+package casuals.filthy.playmaker.backend;
+
+import javax.servlet.http.HttpServlet;
+
+/**
+ * Created by Chris on 3/27/2015.
+ */
+public class GroupUsersServlet extends HttpServlet {
+}
---
+++ MyApplication/backend/src/main/java/casuals/filthy/playmaker/backend/UsersServlet.java
@@ -0,0 +1,14 @@
+/*
+   For step-by-step instructions on connecting your Android application to this backend module,
+   see "App Engine Java Servlet Module" template documentation at
+   https://github.com/GoogleCloudPlatform/gradle-appengine-templates/tree/master/HelloWorld
+*/
+
+package casuals.filthy.playmaker.backend;
+
+import java.io.IOException;
+
+import javax.servlet.http.*;
+
+public class UsersServlet extends HttpServlet {
+}
