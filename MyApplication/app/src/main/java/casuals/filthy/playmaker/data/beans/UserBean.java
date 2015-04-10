52df3d6b2c58de12c81b7f338b52937ed8b3ca09
U-Chris-PC\Chris <chrisf671@gmail.com>
2015-04-10 12:18:49 -0500

    Added support for getting groups through adapter

--- MyApplication/app/src/main/java/casuals/filthy/playmaker/data/beans/UserBean.java
+++ MyApplication/app/src/main/java/casuals/filthy/playmaker/data/beans/UserBean.java
@@ -15,24 +15,12 @@ public class UserBean extends DataBean {
         return id;
     }
 
-    public void setId(String id) {
-        this.id = id;
-    }
-
     public String getEmail() {
         return email;
     }
 
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
     public List<UserGroupBean> getGroups() {
         return groups;
-    }
-
-    public void setGroups(List<UserGroupBean> groups) {
-        this.groups = groups;
     }
 
     @Override
@@ -52,16 +40,8 @@ public class UserBean extends DataBean {
             return name;
         }
 
-        public void setName(String name) {
-            this.name = name;
-        }
-
         public long getId() {
             return id;
-        }
-
-        public void setId(long id) {
-            this.id = id;
         }
 
         @Override
