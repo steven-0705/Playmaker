fa7e1e237964e7439c1b973b88da939fb2db8a25
Shane <shane.mays12@utexas.edu>
2015-04-13 12:19:44 -0500

    Google integration USE GMAIL ONLY

--- MyApplication/app/src/main/java/casuals/filthy/playmaker/MainActivity.java
+++ MyApplication/app/src/main/java/casuals/filthy/playmaker/MainActivity.java
@@ -1,10 +1,12 @@
 package casuals.filthy.playmaker;
+
 import android.app.ActionBar;
 import android.app.ActionBar.Tab;
 import android.app.FragmentTransaction;
 import android.app.Notification;
 import android.app.Service;
 import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
 import android.support.v4.view.ViewPager;
@@ -12,13 +14,27 @@ import android.support.v4.view.ViewPager;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.inputmethod.InputMethodManager;
+import android.widget.Button;
 import android.widget.CheckBox;
-import android.widget.EditText;
-import android.widget.PopupMenu;
-import android.widget.TextView;
-import android.widget.Toast;
 
-public class MainActivity extends FragmentActivity implements ActionBar.TabListener{
+import com.google.android.gms.common.ConnectionResult;
+
+import com.google.android.gms.common.GooglePlayServicesUtil;
+
+import com.google.android.gms.common.SignInButton;
+
+import com.google.android.gms.common.api.GoogleApiClient;
+
+import com.google.android.gms.common.api.GoogleApiClient.ConnectionCallbacks;
+
+import com.google.android.gms.common.api.GoogleApiClient.OnConnectionFailedListener;
+
+import com.google.android.gms.plus.Plus;
+
+import com.google.android.gms.plus.model.people.Person;
+
+
+public class MainActivity extends BaseActivity implements ActionBar.TabListener{
     ActionBar actionbar;
     ViewPager viewpager;
     FragmentPageAdapter ft;
@@ -26,7 +42,7 @@ public class MainActivity extends FragmentActivity implements ActionBar.TabListe
 
     private static CheckBox checkBox1, checkBox2, checkBox3, checkBox4;
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.activity_main);
@@ -37,29 +53,28 @@ public class MainActivity extends FragmentActivity implements ActionBar.TabListe
         actionbar.setDisplayUseLogoEnabled(false);
         actionbar.setDisplayShowCustomEnabled(true);
         actionbar.setDisplayShowHomeEnabled(false);
-
         viewpager.setAdapter(ft);
         actionbar.setNavigationMode(ActionBar.NAVIGATION_MODE_TABS);
+        actionbar.addTab(actionbar.newTab().setText("My Profile").setTabListener(this));
         actionbar.addTab(actionbar.newTab().setIcon(R.drawable.group_icon).setText("Group").setTabListener(this));
-
         actionbar.addTab(actionbar.newTab().setIcon(R.drawable.event_icon).setText("Events").setTabListener(this));
         actionbar.addTab(actionbar.newTab().setIcon(R.drawable.leaderboard_icon).setText("Leader Board").setTabListener(this));
         viewpager.setOnPageChangeListener(new ViewPager.OnPageChangeListener() {
             @Override
             public void onPageSelected(int arg0) {
                 actionbar.setSelectedNavigationItem(arg0);
-
+                checkforSignOut();
             }
             @Override
             public void onPageScrolled(int arg0, float arg1, int arg2) {
                 // TODO Auto-generated method stub
-
+                checkforSignOut();
 
             }
             @Override
             public void onPageScrollStateChanged(int arg0) {
                 // TODO Auto-generated method stub
-
+                checkforSignOut();
             }
         });
 
@@ -70,25 +85,54 @@ public class MainActivity extends FragmentActivity implements ActionBar.TabListe
     @Override
     public void onTabReselected(Tab tab, FragmentTransaction ft) {
         // TODO Auto-generated method stub
-
+        checkforSignOut();
 
     }
     @Override
     public void onTabSelected(Tab tab, FragmentTransaction ft) {
         viewpager.setCurrentItem(tab.getPosition());
-
+        checkforSignOut();
 
     }
     @Override
     public void onTabUnselected(Tab tab, FragmentTransaction ft) {
         // TODO Auto-generated method stub
+        checkforSignOut();
 
+    }
+    public static Button mSignOut;
+    public void checkforSignOut(){
+        if(mSignOut == null)
+        {
+            try{
 
+                mSignOut = (Button) findViewById(R.id.button);
+                mSignOut.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        BaseActivity.login=3;
+                        mSignOut = null;
+                        signOut();
+                        Intent i = new Intent(getApplicationContext(), BaseActivity.class);
+                        startActivity(i);
+                        finish();
+                    }
+                });
+            mSignOut=null;
+            }
+            catch(Exception e)
+            {
+                return;
+            }
+        }
     }
 
 
-
+    @Override
+    public void signOut() {
+        super.signOut();
     }
+}
 
 
 
---
+++ MyApplication/app/src/main/java/casuals/filthy/playmaker/UserFragment.java
@@ -0,0 +1,47 @@
+package casuals.filthy.playmaker;
+
+import android.content.Intent;
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.CheckBox;
+
+/**
+ * Created by Shane on 3/27/2015.
+ */
+public class UserFragment extends Fragment{
+private CheckBox checkBox1, checkBox2, checkBox3, checkBox4;
+        @Override
+        public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                                 Bundle savedInstanceState) {
+            View userView = inflater.inflate(R.layout.user, container,false);
+            Bundle args = getArguments();
+
+            // TODO Auto-generated method stub
+            return userView;
+        }
+
+//        @Override
+//        public void onViewCreated(View view, Bundle savedInstanceState)
+//        {
+//            super.onViewCreated(view, savedInstanceState);
+//            Button test = (Button) getView().findViewById(R.id.button);
+//            test.setOnClickListener(new View.OnClickListener() {
+//                @Override
+//                public void onClick(View v) {
+//                    BaseActivity.login=3;
+//
+//                    Intent i = new Intent(getActivity().getApplicationContext(), BaseActivity.class);
+//                    startActivity(i);
+//
+//                    getActivity().finish();
+//
+//
+//                }
+//            });
+//        }
+
+}
