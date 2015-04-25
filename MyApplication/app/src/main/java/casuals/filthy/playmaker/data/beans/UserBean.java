c2ca76b7d3defed2634e964f35ffa5bdf6a96509
U-Chris-PC\Chris <chrisf671@gmail.com>
2015-04-25 16:32:30 -0500

    minor fix

--- MyApplication/app/src/main/java/casuals/filthy/playmaker/data/beans/UserBean.java
+++ MyApplication/app/src/main/java/casuals/filthy/playmaker/data/beans/UserBean.java
@@ -5,20 +5,12 @@ import java.util.List;
 
 public class UserBean extends DataBean{
 
-    public String id;
-    public String email;
-    public List<UserGroupBean> groups;
-    public List<Invite> invites;
+    protected String id;
+    protected String email;
+    protected List<UserGroupBean> groups;
+    protected List<Invite> invites;
 
     private UserBean() {};
-
-    public UserBean(String email, String name, String user_id) {
-        this.id = user_id;
-        this.name = name;
-        this.email = email;
-
-        groups = new ArrayList<UserGroupBean>();
-    }
 
     public String getId() {
         return id;
@@ -45,7 +37,7 @@ public class UserBean extends DataBean{
                 '}';
     }
 
-    protected static class UserGroupBean {
+    public static class UserGroupBean {
         public String name;
         public long id;
 
@@ -63,7 +55,7 @@ public class UserBean extends DataBean{
         }
     }
 
-    protected static class Invite {
+    public static class Invite {
         public String inviter;
         public long groupId;
         public long date;
